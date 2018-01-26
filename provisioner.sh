5dd58cd4380f854052052dd770eea40d4feeb22b
Luke Cahill <luke.cahill@capgemini.com>
2018-01-26 12:42:29 +0000

    Fix broken Shell file

--- provisioner.sh
+++ provisioner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 # This is assuming that the user is using Ubuntu
-sudo atp-get update -y
+sudo apt-get update -y && apt-get upgrade -y
 sudo apt-get install -y java
 
+# Change the timezone
 sudo unlink /etc/localtime 
 sudo ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
 
